52e22c50a3517cb1effb1151705f578fd21333f1
qumo <andrik4239@gmail.com>
2020-08-22 02:44:39 +0300

    Init repository

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vendor_name/student-list',
+  ),
+  'versions' => 
+  array (
+    'vendor_name/student-list' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
