2f97cc5663db4d8e5c7732e331f5aee4a1e692ae
zadrot1na <zadrot1na65@gmail.com>
2021-01-10 07:28:12 +0200

    StudentRepository->find //works showStudents //works

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -11,7 +11,8 @@ class StudentController
 
     public function showStudents()
     {
-        $connection = new PDO('mysql:host=mysql;dbname=students;charset=utf8', 'root', 'symfony');
+        $connection = new PDO('mysql:host=mysql;dbname=students;charset=utf8',
+            'root', 'symfony');
 
         $students = $connection->query('SELECT * FROM `students`');
 
@@ -27,32 +28,54 @@ class StudentController
     {
         echo 'register handler';
 
-        $student = new Student($_POST['InputName'], $_POST['InputSurname'], $_POST['gender'],
-            $_POST['InputAge'], $_POST['InputGroupNumber'], $_POST['InputMail'], $_POST['InputScore'],
-            $_POST['InputDob'], $_POST['IsLocalCheck']);
-      //  echo (App\Repositoty\StudentRepository)->addStudent($student) . 'registration successful';
+        $temp = [
+            $_POST['InputName'],
+            $_POST['InputSurname'],
+            $_POST['gender'],
+            $_POST['InputAge'],
+            $_POST['InputGroupNumber'],
+            $_POST['InputMail'],
+            $_POST['InputScore'],
+            $_POST['InputDob'],
+            $_POST['IsLocalCheck']
+        ];
 
+        foreach ($temp as $value) {
+            $value = trim($value);
+        }
+
+        $student = new Student(
+            $temp[0],
+            $temp[1],
+            $temp[2],
+            $temp[3],
+            $temp[4],
+            $temp[5],
+            $temp[6],
+            $temp[7],
+            $temp[8]
+        );
         $repos = new StudentRepository();
-        echo $repos->addStudent($student);
+        echo $repos->addStudent($student) . 'registration successful';;
     }
 
-    public function create()
-    {
-        echo 'regiser page';
-
-
-        die();
-        $student = new Student('john', 'miller', 'male', 15, 17, 'john@mail.com', 175, 2000, false);
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST')
-            echo 'NOW ITS POST';
-        else
-            include __DIR__ . '/../View/register.html';
-
-
-        $StudentGateway = new StudentRepository();
-        $StudentGateway->addStudent($student);
-    }
+//    public function create()
+//    {
+//        echo 'regiser page';
+//
+//
+//        die();
+//        $student = new Student('john', 'miller', 'male', 15, 17, 'john@mail.com', 175, 2000, false);
+//
+//        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+//            echo 'NOW ITS POST';
+//        else
+//            include __DIR__ . '/../View/register.html';
+//
+//
+//        $StudentGateway = new StudentRepository();
+//        $StudentGateway->addStudent($student);
+//    }
 
     public function remove()
     {
